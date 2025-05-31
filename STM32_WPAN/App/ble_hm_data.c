12fdf7fdf54fbcb4371757931f0782be866a80b0
mgarvelaiz <mg.arvelaiz@alumnos.upm.es>
2025-05-31 16:23:53 +0200

    files to handle the custom service

---
+++ STM32_WPAN/App/ble_hm_data.c
@@ -0,0 +1,8 @@
+/*
+ * ble_hm_data.c
+ *
+ *  	Created on: May, 2025
+ *      Author: Mariana Arvelaiz
+ */
+
+
---
+++ STM32_WPAN/App/ble_hm_data.h
@@ -0,0 +1,13 @@
+/*
+ * ble_hm_data.h
+ *
+ *  Created on: May 30, 2025
+ *      Author: magaa
+ */
+
+#ifndef APP_BLE_HM_DATA_H_
+#define APP_BLE_HM_DATA_H_
+
+
+
+#endif /* APP_BLE_HM_DATA_H_ */
