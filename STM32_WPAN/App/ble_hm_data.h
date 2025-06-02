00cd4e5d4bc066d7397c2f39601988c9794b120e
mgarvelaiz <mg.arvelaiz@alumnos.upm.es>
2025-06-03 00:47:50 +0200

    Interface to handle and trigger notifications

--- STM32_WPAN/App/ble_hm_data.h
+++ STM32_WPAN/App/ble_hm_data.h
@@ -1,13 +1,44 @@
 /*
  * ble_hm_data.h
  *
- *  Created on: May 30, 2025
- *      Author: magaa
+ *  Created on: May, 2025
+ *      Author: Mariana Arvelaiz
  */
 
 #ifndef APP_BLE_HM_DATA_H_
 #define APP_BLE_HM_DATA_H_
 
+/* Includes ------------------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "custom_stm.h"
 
+
+/* USER CODE END Includes */
+
+
+typedef enum {
+	Notify_None,
+	Notify_Pending
+} BLE_NotifyChar_Status;
+
+/* USER CODE BEGIN ET */
+typedef struct
+{
+  uint8_t	pck_id;
+  uint8_t	type;
+  uint8_t	data [8];
+  uint8_t	checkSum;
+  uint8_t	pck[11];
+  uint8_t	length;
+} Custom_BLE_Payload_t;
+
+
+typedef struct
+{
+  BLE_NotifyChar_Status	Status;
+  uint8_t*	pck;
+  uint8_t	checkSum;
+  uint8_t	length;
+} Custom_BLE_Notify_interface_t;
 
 #endif /* APP_BLE_HM_DATA_H_ */
