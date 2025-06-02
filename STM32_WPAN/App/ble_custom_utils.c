e943030111ffecf9e2285dee18cb5bc91df7b28e
mgarvelaiz <mg.arvelaiz@alumnos.upm.es>
2025-06-03 00:47:23 +0200

    Check Sum Calculation added

---
+++ STM32_WPAN/App/ble_custom_utils.c
@@ -0,0 +1,21 @@
+/*
+ * ble_custom_utils.c
+ *
+ *  Created on: Jun 3, 2025
+ *      Author: magaa
+ */
+
+
+
+
+uint8_t ble_calculate_chksum(uint8_t *pck, uint8_t length){
+	uint8_t chksum=0x00;
+	uint16_t i;
+
+
+	for(i=0; i<length; i++){
+		chksum= (*(pck+i)) +chksum;
+	}
+
+	return chksum;
+}
---
+++ STM32_WPAN/App/ble_custom_utils.h
@@ -0,0 +1,15 @@
+/*
+ * ble_custom_utils.h
+ *
+ *  Created on: Jun 3, 2025
+ *      Author: magaa
+ */
+
+#ifndef APP_BLE_CUSTOM_UTILS_H_
+#define APP_BLE_CUSTOM_UTILS_H_
+
+
+
+uint8_t ble_calculate_chksum(uint8_t *pck, uint8_t length);
+
+#endif /* APP_BLE_CUSTOM_UTILS_H_ */
